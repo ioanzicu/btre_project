6452e650bf2165d76a9cf45fa8d339d9947a061b
Ioan Zicu <ioan.zicu94@gmail.com>
2020-11-17 05:39:13 +0100

    dashboard functionality

--- contacts/views.py
+++ contacts/views.py
@@ -32,14 +32,14 @@ def contact(request):
         contact.save()
 
         # Send email
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing +
-            '. Sign into the admin panel or more info.',
-            '',  # Enter your proxy email that will send mail
-            [realtor_email, 'vanea-zicu@mail.ru'],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing +
+        #     '. Sign into the admin panel or more info.',
+        #     '',  # Enter your proxy email that will send mail
+        #     [realtor_email, 'vanea-zicu@mail.ru'],
+        #     fail_silently=False
+        # )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
